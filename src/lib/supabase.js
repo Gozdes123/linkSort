a0125e62a9806906e9b1aaffb007879ac8f3d194
WILSON <reborn2616am@gmail.com>
2026-02-23 22:17:26 +0800

    feat:新增環境變數

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vnzolmsjftcqcyrvhall.supabase.co'
-const supabaseKey = 'sb_publishable_SaLhF_xj1c4SLl1S3ww-Ag_9odbsPho'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
